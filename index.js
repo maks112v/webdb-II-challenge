59526e2367016f5dfcc1776541611c5f1f9792e7
Maksim <maks112v@gmail.com>
2019-03-26 19:57:28 -0400

    finished mvp

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
-const knex = require('knex');
-const db = knex("./knexfile.js");
+const knex = require("knex");
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  }
+};
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -10,12 +17,76 @@ server.use(helmet());
 
 // endpoints here
 server.get("/", (req, res) => {
-  db("lambda")
-    .then(res => {
-      console.log(res)
+  db("zoos")
+    .then(zoo => {
+      console.log(zoo);
+      res.status(200).json(zoo);
     })
     .catch(err => {
-      console.log(err)
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+server.get("/:id", (req, res) => {
+  const zooid = req.params.id;
+  db("zoos")
+    .where({ id: zooid })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ message: "Item not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post("/", (req, res) => {
+  db("zoos")
+    .insert(req.body)
+    .then(arr => {
+      res.status(201).json(arr);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+server.put("/:id", (req, res) => {
+  const zooid = req.params.id;
+  db("zoos")
+    .where({ id: zooid })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: "Not Found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete("/:id", (req, res) => {
+  const zooid = req.params.id;
+  db("zoos")
+    .where({ id: zooid })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: "Not Found" });
+      }
+    })
+    .catch(err => {
       res.status(500).json(err);
     });
 });
--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,44 @@
 // Update with your config settings.
 
 module.exports = {
-  client: "sqlite3",
-  useNullAsDefault: true,
-  connection: {
-    filename: "./data/lambda.db3"
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
+
 };
